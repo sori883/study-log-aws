eed672b66c05c3b774ec7634ce09edad8e138b88
sorinaji <37400002+sori883@users.noreply.github.com>
2025-03-05 23:54:59 +0900

    JWTを検証してコンテキストに詰めるまで作成

--- apps/web/auth/jwt.ts
+++ apps/web/auth/jwt.ts
@@ -1,7 +1,10 @@
 import { CognitoJwtVerifier } from "aws-jwt-verify";
+import { CognitoIdTokenPayload } from "aws-jwt-verify/jwt-model";
 
 export const jwtVerifier = CognitoJwtVerifier.create({
   userPoolId: process.env.COGNITO_USER_POOL_ID!,
   tokenUse: "id",
   clientId: process.env.COGNITO_CLIENT_ID!,
 });
+
+export type JwtPayload = CognitoIdTokenPayload;
--- apps/web/server/index.ts
+++ apps/web/server/index.ts
@@ -1,15 +1,19 @@
 import { Hono } from "hono";
 import { getCookie } from "hono/cookie";
-import { jwtVerifier } from '../auth/jwt';
+import { jwtVerifier, JwtPayload } from '../auth/jwt';
+
 
 const app = new Hono();
 
-app.use(async (c, next) => {
+app.use<{ Variables: { jwt: JwtPayload }}>(async (c, next) => {
+  // トークンの取得
   const token = getCookie(c, "id_token");
   if (!token) return c.redirect("/login");
 
+  // トークンの検証
   await jwtVerifier.hydrate();
   const jwtPayload = await jwtVerifier.verify(token);
+  c.set("jwt", jwtPayload);
 
   await next();
   c.header("X-Powered-By", "React Router and Hono");
