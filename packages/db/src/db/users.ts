8caa2e878838a18a12ed8b78cf538f2b39633738
sorinaji <37400002+sori883@users.noreply.github.com>
2025-03-24 22:51:34 +0900

    dbスキーマ途中

--- packages/db/src/db/users.ts
+++ packages/db/src/db/users.ts
@@ -1,13 +1,25 @@
-import { sql } from "drizzle-orm";
+import { relations, sql } from "drizzle-orm";
 import { createCaTable } from "./_table";
 import { integer, text } from "drizzle-orm/sqlite-core";
+import { profilesTable } from "./profiles";
+import { careersTable } from "./careers";
 
 export const usersTable = createCaTable("users_table", {
-  id: integer().primaryKey({ autoIncrement: true }),
-  username: text().notNull(),
-  email: text().notNull().unique(),
-  createdAt: text("created_at").default(sql`(CURRENT_TIMESTAMP)`).notNull(),
+  id: integer("id", { mode: "number" }).primaryKey({ autoIncrement: true }),
+  username: text("username").notNull(),
+  displayName: text("display_name").notNull(),
+  email: text("email").notNull().unique(),
+  thumbnailUrl: text("thumbnail_url"),
+  profileId: integer("profile_id", { mode: "number" }),
+  createdAt: text().default(sql`(CURRENT_TIMESTAMP)`).notNull(),
   updatedAt: text("updated_at").$onUpdate(() => sql`(CURRENT_TIMESTAMP)`),
   deletedAt: text("deleted_at").default(sql`NULL`),
 });
 
+export const usersRelations = relations(usersTable, ({ one, many }) => ({
+	profile: one(profilesTable, {
+    fields: [usersTable.profileId],
+    references: [profilesTable.id],
+  }),
+  career: many(careersTable),
+}));
